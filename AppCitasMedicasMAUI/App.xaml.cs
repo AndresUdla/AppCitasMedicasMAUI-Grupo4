52e1276da8a63581b6835ea00b6869e83cb058b8
Josue Mullo <josue.mullo@udla.edu.ec>
2025-05-26 20:31:38 -0500

    Add project files.

---
+++ AppCitasMedicasMAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppCitasMedicasMAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppCitasMedicasMAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppCitasMedicasMAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
