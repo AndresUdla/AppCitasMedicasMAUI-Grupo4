a98d9b2a818f5108832fe39dbd155ee2febaaf38
Josue Mullo <josue.mullo@udla.edu.ec>
2025-06-04 16:47:12 -0500

    Creando la clase Usuario para el consumo de un endpoint que servira para hacer login correctamente Ademas se creo el UsuarioService, para poder consumir la API

---
+++ AppCitasMedicasMAUI/Models/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCitasMedicasMAUI.Models
+{
+    public enum RolUsuario
+    {
+        Administrador,
+        Medico,
+        Paciente
+    }
+
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+        public string Correo { get; set; }
+        public string Contrasena { get; set; }
+        public RolUsuario Rol { get; set; }
+    }
+}
---
+++ AppCitasMedicasMAUI/Service/UsuarioService.cs
@@ -0,0 +1,48 @@
+﻿using AppCitasMedicasMAUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AppCitasMedicasMAUI.Service
+{
+    public class UsuarioService
+    {
+        private readonly HttpClient _httpClient;
+
+        public UsuarioService()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://TU-URL-DE-LA-API/api/");
+        }
+
+        public async Task<Usuario> LoginAsync(string correo, string contrasena)
+        {
+            var request = new
+            {
+                Correo = correo,
+                Contrasena = contrasena
+            };
+
+            var json = JsonSerializer.Serialize(request);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("usuario/login", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+                var usuario = JsonSerializer.Deserialize<Usuario>(responseContent, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+
+                return usuario;
+            }
+
+            return null;
+        }
+    }
+}
