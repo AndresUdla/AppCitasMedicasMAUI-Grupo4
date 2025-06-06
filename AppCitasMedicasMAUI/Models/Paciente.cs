a032e38fb857a42245250d9f8e7eab5f0f881a26
Josue Mullo <josue.mullo@udla.edu.ec>
2025-06-05 20:34:20 -0500

    Añadiendo el modelo Pacientee para el posterior uso en el PacienteService y poder consumir la api

---
+++ AppCitasMedicasMAUI/Models/Paciente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCitasMedicasMAUI.Models
+{
+    internal class Paciente
+    {
+        public int PacienteId { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Cedula { get; set; }
+        public int Edad { get; set; }
+        public double? Altura { get; set; }
+        public double? Peso { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public int UsuarioId { get; set; }
+    }
+}
---
+++ AppCitasMedicasMAUI/Service/PacienteService.cs
@@ -0,0 +1,49 @@
+﻿using AppCitasMedicasMAUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCitasMedicasMAUI.Service
+{
+    internal class PacienteService
+    {
+        private readonly HttpClient _httpClient;
+
+        public PacienteService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://localhost:7062");
+        }
+
+        public async Task<List<Paciente>> ObtenerPacientesAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Paciente>>("Paciente");
+        }
+
+        public async Task<Paciente> ObtenerPacientePorIdAsync(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<Paciente>($"api/Paciente/{id}");
+        }
+
+        public async Task<bool> CrearPacienteAsync(Paciente paciente)
+        {
+            var response = await _httpClient.PostAsJsonAsync("Paciente", paciente);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> ActualizarPacienteAsync(int id, Paciente paciente)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"Paciente/{id}", paciente);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> EliminarPacienteAsync(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"Paciente/{id}");
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
